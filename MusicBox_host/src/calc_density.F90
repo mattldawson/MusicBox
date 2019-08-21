230696e57128f9ee54788da95fed0aea970e8e20
Cheryl Craig <cacraig@ucar.edu>
2019-08-21 14:10:57 -0600

    Removed empty routines and optional=F attributes

--- MusicBox_host/src/calc_density.F90
+++ MusicBox_host/src/calc_density.F90
@@ -8,19 +8,6 @@ module calc_density
   
 contains
 
-!> \section arg_table_calc_density_init Argument Table
-!! \htmlinclude calc_density_init.html
-!!
-  subroutine calc_density_init(errmsg, errflg)
-    character(len=512),     intent(out) :: errmsg
-    integer,                intent(out) :: errflg
-
-    !--- initialize CCPP error handling variables
-    errmsg = ''
-    errflg = 0
-
-  end subroutine calc_density_init
-
 !> \section arg_table_calc_density_run Argument Table
 !! \htmlinclude calc_density_run.html
 !!
@@ -41,20 +28,5 @@ contains
     density = 10._kind_phys*press/(kboltz*temperature)
     
   end subroutine calc_density_run
-
-!> \section arg_table_calc_density_finalize Argument Table
-!! \htmlinclude calc_density_finalize.html
-!!
-  subroutine calc_density_finalize( errmsg, errflg )
-
-    !--- arguments
-    character(len=512), intent(out) :: errmsg
-    integer,            intent(out) :: errflg
-
-    !--- initialize CCPP error handling variables
-    errmsg = ''
-    errflg = 0
-
-  end subroutine calc_density_finalize
   
 end module calc_density
